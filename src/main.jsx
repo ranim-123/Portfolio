16f3cbdc4f71244c24d1bde170f7ef6d0d3c3a74
Ranim Taieb <ranim.taieb@univ-constantine2.dz>
2025-10-28 15:36:45 +0100

    fix the last problem

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './index.css'
-import Home from'@/pages/home'
-import NotFound from '@/pages/notFound'
-import { Toaster } from 'react-hot-toast';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Home from "@/pages/home";
+import NotFound from "@/pages/notFound";
+import { Toaster } from "react-hot-toast";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Home />,
   },
   {
-   path:"*",
-   element:<NotFound/>
+    path: "*",
+    element: <NotFound />,
   },
-])
-createRoot(document.getElementById('root')).render(
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
     <Toaster position="top-right" />
-  </StrictMode>,
-)
+  </StrictMode>
+);
